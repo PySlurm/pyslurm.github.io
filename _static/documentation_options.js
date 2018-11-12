344d681f807963f9951f8e92092b672abbd9bbfe
Travis <Travis>
2018-11-12 15:28:39 +0000

    Updated docs

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '17.11.0.11',
+    VERSION: '17.11.0.14',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
